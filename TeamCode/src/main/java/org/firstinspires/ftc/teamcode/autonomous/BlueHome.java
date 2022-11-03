c5c551818e9e0d7c45f89896fce21dcec5840ceb
Nishant Palasamudram <palasamudramnishant@gmail.com>
2022-11-03 12:18:15 -0500

    parking trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueHome.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueHome.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import org.firstinspires.ftc.robotcore.internal.system.Assert;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -20,7 +19,11 @@ public class BlueHome extends LinearOpMode
 
     private final Pose2d blueHome = new Pose2d(-36, 60, Math.toRadians(270));
 
+
     private TrajectorySequence trajectoryTo12; //check coordinate system in notebook
+    private TrajectorySequence trajectoryToParking3;
+    private TrajectorySequence trajectoryToParking2;
+    private TrajectorySequence trajectoryToParking1;
 
     @Override
     public void runOpMode()
@@ -44,6 +47,10 @@ public class BlueHome extends LinearOpMode
         drive.followTrajectorySequence(trajectoryTo12);
         utilities.wait(12, telemetry);
         utilities.highJunction(telemetry);
+        //if(identifier)
+        {
+
+        }
 
     }
     private void buildTrajectories()
@@ -52,6 +59,20 @@ public class BlueHome extends LinearOpMode
                 .turn(Math.toRadians(90))
                 .forward(24)
                 .strafeRight(34)
+                .build();
+        trajectoryToParking3 = drive.trajectorySequenceBuilder(blueHome)
+                .strafeRight(14)
+                .turn(Math.toRadians(180))
+                .forward(47)
+                .build();
+        trajectoryToParking2 = drive.trajectorySequenceBuilder(blueHome)
+                .strafeRight(14)
+                .turn(Math.toRadians(180))
+                .forward(24)
+                .build();
+        trajectoryToParking1 = drive.trajectorySequenceBuilder(blueHome)
+                .strafeRight(14)
+                .turn(Math.toRadians(180))
                 .build();
     }
 }
