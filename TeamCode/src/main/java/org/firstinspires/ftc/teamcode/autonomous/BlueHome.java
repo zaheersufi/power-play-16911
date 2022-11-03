3921daf0026ba8dbd9836f5da005b5cdbb79f3e3
Zaheer Sufi <zaheerjunaidsufi@gmail.com>
2022-11-02 22:03:39 -0500

    finally back to original

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueHome.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.internal.system.Assert;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.RigatoniHardware;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.RigatoniHardware;
+
+@Autonomous(name="BlueHome")
+public class BlueHome extends LinearOpMode {
+    private SampleMecanumDrive drive;
+    private Utilities utilities;
+
+    private final Pose2d blueHome = new Pose2d(-36, 60, 270);
+
+    @Override
+    public void runOpMode()
+    {
+        RigatoniHardware hardware = new RigatoniHardware(); //Horizontal Claw
+        Assert.assertNotNull(hardwareMap);
+        hardware.initializePrimaryMotors(hardwareMap);
+        hardware.initializeClawServos(hardwareMap);
+        hardware.initializeSupplementaryMotors(hardwareMap);
+
+        utilities = new Utilities(hardware);
+        drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(blueHome);
+        buildTrajectories();
+
+
+
+    }
+    private void buildTrajectories()
+    {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Utilities.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.hardware.RigatoniHardware;
+import org.firstinspires.ftc.teamcode.hardware.RigatoniHardware;
+
+//Horizontal Claw Utilities
+public class Utilities
+{
+
+    private RigatoniHardware hardware;
+    Utilities(RigatoniHardware hardware)
+    {
+        this.hardware = hardware;
+    }
+    public void openClaw()
+    {
+        hardware.grabServo.setPosition(0.2);
+        hardware.grabServo.setPosition(0.0);
+    }
+    //    public void wait(int waitTime, Telemetry telemetry)
+//    {
+//        ElapsedTime time = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+//        time.reset();
+//        while (time.time() < waitTime)
+//        {
+//            telemetry.addData("Status", "Waiting");
+//            telemetry.addData("Wait Time", waitTime / 1000);
+//            telemetry.addData("Time Left", (waitTime - time.time()) / 1000);
+//            telemetry.update();
+//        }
+//    }
+    public void rotateClaw(int position)
+    {
+        hardware.rotServo.setPosition (position);
+    }
+    public void liftArm(String pos)
+    {
+        switch(pos)
+        {
+            case "floor":
+                //hardwareMap.liftArm.
+                break;
+            case "low":
+                break;
+            case "medium":
+                break;
+            case "high":
+
+        }
+
+    }
+}
