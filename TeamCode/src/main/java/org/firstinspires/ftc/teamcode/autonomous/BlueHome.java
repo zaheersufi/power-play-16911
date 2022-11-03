4dd36a9551d18105eeb70287a49388745514c2d4
Nishant Palasamudram <palasamudramnishant@gmail.com>
2022-11-03 11:54:44 -0500

    zahbaka

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueHome.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueHome.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import org.firstinspires.ftc.robotcore.internal.system.Assert;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -41,7 +42,7 @@ public class BlueHome extends LinearOpMode
         utilities.wait(initialWaitTime, telemetry);
 
         drive.followTrajectorySequence(trajectoryTo12);
-
+        utilities.wait(12, telemetry);
         utilities.highJunction(telemetry);
 
     }
