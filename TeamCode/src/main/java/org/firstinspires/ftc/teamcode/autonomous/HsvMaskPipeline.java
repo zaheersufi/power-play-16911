d160bae1c7c9ae57f145c43831943de2c88ac8e2
lawrencegranda <95052347+lawrencegranda@users.noreply.github.com>
2022-12-07 13:32:04 -0600

    Update HsvMaskPipeline.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/HsvMaskPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/HsvMaskPipeline.java
@@ -54,9 +54,9 @@ public class HsvMaskPipeline extends OpenCvPipeline
     /**
      * Core values that define the location and size of the region
      */
-    static final Point REGION_TOPLEFT_POINT = new Point(50,110);  // x,y (top left corner of the window is (0,0))
-    static final int REGION_WIDTH = 35;
-    static final int REGION_HEIGHT = 35;
+    static final Point REGION_TOPLEFT_POINT = new Point(50,90);  // x,y (top left corner of the window is (0,0))
+    static final int REGION_WIDTH = 40;
+    static final int REGION_HEIGHT = 60;
 
 
     /**
