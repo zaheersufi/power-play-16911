300319a667722ed183306b10dabea9db6a477ff2
lawrencegranda <95052347+lawrencegranda@users.noreply.github.com>
2022-12-09 16:40:22 -0600

    Parking-only autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ParkingRight.java
@@ -0,0 +1,169 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.internal.system.Assert;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.RigatoniHardware;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name="ParkingRight")
+public class ParkingRight extends LinearOpMode
+{
+    private SampleMecanumDrive drive;
+    private Utilities utilities;
+    private RigatoniHardware hardware;
+    private OpenCvInternalCamera webcam;
+    private HsvMaskPipeline pipeline;
+
+
+    private final Pose2d blueHome = new Pose2d(-36, 60, Math.toRadians(270));
+
+
+    private TrajectorySequence trajectoryTo12; //check coordinate system in notebook
+    private TrajectorySequence trajectoryToParking3;
+    private TrajectorySequence trajectoryToParking2;
+    private TrajectorySequence trajectoryToParking1;
+
+
+    private final int initialWaitTime = 250;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+
+        pipeline = new HsvMaskPipeline(telemetry);
+        webcam.setPipeline(pipeline);
+
+        webcam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener(){
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_RIGHT);
+            }
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("ERROR", "*Camera could not be opened* "+errorCode);
+                telemetry.update();
+            }
+        });
+
+
+        Assert.assertNotNull(hardwareMap);
+
+        hardware = new RigatoniHardware();
+        hardware.initializePrimaryMotors(hardwareMap);
+        hardware.initializeClawServos(hardwareMap);
+        hardware.initializeSupplementaryMotors(hardwareMap);
+        utilities = new Utilities(hardware);
+
+        drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(blueHome);
+
+        turnOnEncoders();
+
+
+        buildTrajectories();
+        utilities.openClaw(false);
+
+        waitForStart();
+        if(!opModeIsActive()) {return;}
+
+        utilities.wait(initialWaitTime, telemetry);
+
+
+        final int identifier = pipeline.getDestination();
+        telemetry.addData("Parking", identifier);
+        telemetry.update();
+
+
+        drive.followTrajectorySequence(trajectoryTo12);
+
+        if(identifier == 1)
+            drive.followTrajectorySequence(trajectoryToParking1);
+        else if (identifier == 2)
+            drive.followTrajectorySequence(trajectoryToParking2);
+        else
+            drive.followTrajectorySequence(trajectoryToParking3);
+
+    }
+
+
+
+    private void buildTrajectories()
+    {
+        trajectoryTo12 = drive.trajectorySequenceBuilder(blueHome)
+                .forward(6)
+                .turn(Math.toRadians(90))
+                .forward(19)
+                .strafeRight(38)
+                .forward(5)
+                .build();
+
+        trajectoryToParking1 = drive.trajectorySequenceBuilder(trajectoryTo12.end())
+                .strafeRight(12)
+                .turn(Math.toRadians(180))
+                .build();
+
+        trajectoryToParking2 = drive.trajectorySequenceBuilder(trajectoryTo12.end())
+                .strafeRight(12)
+                .turn(Math.toRadians(180))
+                .forward(22)
+                .build();
+
+        trajectoryToParking3 = drive.trajectorySequenceBuilder(trajectoryTo12.end())
+                .strafeRight(12)
+                .turn(Math.toRadians(180))
+                .forward(44)
+                .strafeRight(1)
+                .build();
+
+    }
+
+
+
+    private void turnOnEncoders()
+    {
+        hardware.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.liftArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+
+
+    public void setUpCamera(HsvMaskPipeline pipeline)
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        webcam.setPipeline(pipeline);
+        webcam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener(){
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_RIGHT);
+            }
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("ERROR", "*Camera could not be opened* "+errorCode);
+                telemetry.update();
+            }
+        });
+
+    }
+}
