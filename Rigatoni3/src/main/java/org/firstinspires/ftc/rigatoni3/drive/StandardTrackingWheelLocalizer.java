546a5cc3c73e03de9a708999a7606332ce469f1c
Zaheer Sufi <zaheerjunaidsufi@gmail.com>
2023-05-10 10:20:12 -0500

    dead wheels set up TODO: add dead wheels to config

--- Rigatoni3/src/main/java/org/firstinspires/ftc/rigatoni3/drive/StandardTrackingWheelLocalizer.java
+++ Rigatoni3/src/main/java/org/firstinspires/ftc/rigatoni3/drive/StandardTrackingWheelLocalizer.java
@@ -31,8 +31,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 12; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 2.5; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
@@ -48,6 +48,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontEncoder"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
+        rightEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
